2a31cd3b50329cb1850752bd478b2093d8581002
Ctibor Laky <luckylooke@gmail.com>
2016-10-17 14:36:23 +0200

    sass/scss support

--- definitions/merge.js
+++ definitions/merge.js
@@ -1,2 +1,2 @@
-F.merge('/css/bundle.css', '/css/bootstrap.min.css', '/css/ui.css', '/css/index.css', '/css/default.css');
+F.merge('/css/bundle.css', '/css/bootstrap.min.css', '/css/ui.css', '/css/index.css', '/css/default.css', 'css/test.scss');
 F.merge('/js/bundle.js', '/js/jctajr.min.js', '/js/ui.js', '/js/default.js');
