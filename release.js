72a332bbde877815f6584eb422dfb892cd764ea1
luckylooke <luckylooke@gmail.com>
2016-11-09 07:45:07 +0000

    release setup edit

--- release.js
+++ release.js
@@ -8,8 +8,9 @@ var fs = require('fs');
 var options = {};
 
 // options.ip = '127.0.0.1';
-options.ip = '212.47.251.17';
-options.port = parseInt(process.argv[2]);
+options.ip = process.env.HOST;
+// options.port = parseInt(process.argv[2]);
+options.port = process.env.PORT;
 // options.config = { name: 'total.js' };
 // options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
 // options.sleep = 2000;
