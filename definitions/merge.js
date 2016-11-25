7f09df7f80ca4488d7666f3f0c18f3a86d43bdc4
Matus Medzay <matus@medzay.sk>
2016-11-25 10:27:28 +0100

    Added form style and favicon.

--- definitions/merge.js
+++ definitions/merge.js
@@ -1,3 +1,3 @@
-F.merge('/css/bundle.css', '/css/bootstrap.min.css', '/css/ui.css', '/css/animate.css', '/css/default.css', '/css/portal.scss');
+F.merge('/css/bundle.css', '/css/bootstrap.min.css', '/css/animate.css', '/css/default.css', '/css/portal.scss');
 F.merge('/js/bundlehead.js', '/js/wow.min.js', '/js/portalhead.js', '/js/jctajr.min.js');
 F.merge('/js/bundlebody.js', '/js/JS.Responsive.full.js', '/js/ui.js', '/js/default.js', '/js/fastclick.js', '/js/jquery.parallax.min.js', '/js/portal.js');
